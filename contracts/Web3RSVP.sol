d0d687514e653aec9ad406ceee62c7433ca7a79f
Abidan Triguero <abidantriguero@gmail.com>
2022-08-04 17:11:04 -0400

    End of Day 3

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,2 +1,19 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
+
+contract Web3RSVP {
+   struct CreateEvent {
+       bytes32 eventId;
+       string eventDataCID;
+       address eventOwner;
+       uint256 eventTimestamp;
+       uint256 deposit;
+       uint256 maxCapacity;
+       address[] confirmedRSVPs;
+       address[] claimedRSVPs;
+       bool paidOut;
+   }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+
+}
