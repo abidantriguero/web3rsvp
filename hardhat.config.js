dbcf4f9b405ac8ab0b9286c51219e45f3ebd3f5b
Abidan Triguero <abidantriguero@gmail.com>
2022-08-16 15:15:21 -0400

    Deploying Smart Contract Day 6 Section 4

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,29 @@
-require("@nomiclabs/hardhat-waffle");
+/** @type import('hardhat/config').HardhatUserConfig */
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-chai-matchers")
+require("@nomiclabs/hardhat-ethers")
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygonMumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+    }
+  }
+
 };
